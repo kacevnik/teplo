2d5a1fc00f0aff7b9ce83156888337e35c7197a1
kacevnik <kacevnik@yandex.ru>
2017-09-24 17:09:29 +0300

    Следущий блок с опросом

--- js/script.js
+++ js/script.js
@@ -36,6 +36,54 @@ jQuery(document).ready(function ($) {
 
 
   	//Маска под телефонный номер для поля Input
-  	$('input[name="phone"]').mask("+7 (999) 999-99-99");
+  	$('input[name="phone"]').mask("+7 (999) 999-99-99"); 	
+
+  	$('#select_body .select_body_show .item a').click(function(event) {
+  		$('#select_body .select_body_show .item a').removeClass('selected');
+  		$(this).addClass('selected');
+  		if($('#more').attr('data_to') == 'step_2'){
+  			$('input[name="home"]').val($(this).html());
+  		}  		
+  		if($('#more').attr('data_to') == 'step_3'){
+  			$('input[name="fuel"]').val($(this).html());
+  		}
+  		$('#more').attr('data_ok', 'ok');
+  		return false;
+  	});
+
+  	$('#select_body input').keyup(function(event) {
+  		if($(this).val() != ''){
+  			$('#more').attr('data_ok', 'ok');
+  		}
+  		else{
+  			$('#more').attr('data_ok', '');
+  		}
+  	});
+
+
+  	$('.link').click(function(event) {
+  		var step = $(this).attr('data_to').split('_')[1];
+	  	if($('#more').attr('data_ok') == 'ok'){
+	  		$('#select .answer ul li[data-activ='+step+']').addClass('activ');
+
+	  		$('.select_body_show').hide();
+	  		$('#step_' + step).show();
+	  		$('.link').attr('data_to', 'step_' + (step*1 + 1));
+
+	  		$('#more').attr('data_ok', '');
+	  		$(this).blur();
+	  		$(this).next().hide();
+
+	  		if(step == 4){
+	  			$(this).hide();
+	  			$('#select_body input[name="submit"]').show();
+	  		}	  		
+
+	  		if(step == 5){
+	  			$('#select_body input[name="submit"]').hide();
+	  		}
+	  	}
+  		return false;
+  	});
 
 });
